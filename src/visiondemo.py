819ba7c1a9e7436b959d47dbe7977a3ce40e51bd
avikboss <abosshar@macalester.edu>
2019-06-28 11:18:26 -0500

    cleaned up unncessary comments

--- src/visiondemo.py
+++ src/visiondemo.py
@@ -6,12 +6,7 @@ Author: Amy McGovern
 """
 from pyparrot.Bebop import Bebop
 from pyparrot.DroneVisionGUI import DroneVisionGUI
-import threading
 import cv2
-import time
-from PyQt5.QtGui import QImage
-from datetime import datetime
-import sys
 from src.bebop_teleop import Keybop
 isAlive = False
 
@@ -26,54 +21,16 @@ class UserVision:
         # cv2.imwrite("test"+datetime.now().strftime("%H%M%S")+".png",img)
 
 
-
-
-def draw_current_photo():
-    """
-    Quick demo of returning an image to show in the user window.  Clearly one would want to make this a dynamic image
-    """
-    image = cv2.imread('test_image_000001.png')
-
-    if (image is not None):
-        if len(image.shape) < 3 or image.shape[2] == 1:
-            image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-        else:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        height, width, byteValue = image.shape
-        byteValue = byteValue * width
-
-        qimage = QImage(image, width, height, byteValue, QImage.Format_RGB888)
-
-        return qimage
-    else:
-        return None
-
 def demo_user_code_after_vision_opened(bebopVision, args):
     bebop = args[0]
 
     print("Vision successfully started!")
-    #removed the user call to this function (it now happens in open_video())
-    #bebopVision.start_video_buffering()
-
-    # takeoff
-    # bebop.safe_takeoff(5)
-
-    # skipping actually flying for safety purposes indoors - if you want
-    # different pictures, move the bebop around by hand
-    print("Fly me around by hand!")
     bebop.smart_sleep(1)
 
     if bebopVision.vision_running:
         print("Moving the camera using velocity")
 
         Keybop(bebop).start()
-        # bebop.pan_tilt_camera_velocity(pan_velocity=0, tilt_velocity=-2, duration=2)
-        # bebop.smart_sleep(30)
-        # bebop.fly_direct(pitch=0,roll=0,yaw=0,vertical_movement=0,duration=12)
-        # bebop.fly_direct(pitch=0, roll=0, yaw=100, vertical_movement=0, duration=12)
-        # land
-        # bebop.safe_land(5)
 
         print("Finishing demo and stopping vision")
         bebopVision.close_video()
@@ -93,7 +50,7 @@ if __name__ == "__main__":
     if success:
         # start up the video
         bebopVision = DroneVisionGUI(bebop, is_bebop=True, user_code_to_run=demo_user_code_after_vision_opened,
-                                     user_args=(bebop, ))#, user_draw_window_fn=draw_current_photo)
+                                     user_args=(bebop, ))
 
         userVision = UserVision(bebopVision)
         bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
